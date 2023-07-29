d9acc1d0eceaad375629b9c15026ae8145031d0f
Daniel Cui <solunian@gmail.com>
2023-07-29 02:05:36 -0400

    add license, create barebones interface

---
+++ interface/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let keys: Set<string> = new Set();
+
+	onMount(() => {
+		window.addEventListener('keypress', (event) => {
+			keys.add(event.key);
+			keys = keys;
+		});
+
+		window.addEventListener('keyup', (event) => {
+			keys.delete(event.key);
+			keys = keys;
+		});
+	});
+</script>
+
+<div class="flex w-full flex-row justify-center">
+	<div class="flex h-screen flex-col justify-center">
+		<div class="flex flex-row flex-wrap gap-8 p-4 text-4xl">
+			{#each keys as key}
+				<span>{key}</span>
+			{/each}
+		</div>
+	</div>
+</div>
